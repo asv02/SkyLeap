f06580b9034a3392e082404c09c979fa1abe222d
asv02 <200106025@hbtu.ac.in>
2025-10-14 08:23:41 +0530

    added foreign keys conttaints on flights and airports

--- models/city.js
+++ models/city.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
       this.hasMany(models.Airport, {
         foreignKey: "cityId",
         onDelete: "CASCADE",
-        onUpdate: "CASCADE",
       });
     }
   }
